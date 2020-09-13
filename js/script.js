542b45afa562a100882775aa0d64ca189136c746
MihailDezhko <mihaildezhko@gmail.com>
2020-09-13 17:02:08 +0300

    finished

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-var $page = $('html, body');
-$('a[href*="#"]').click(function () {
-    $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 1200);
-    return false;
+var burger = document.querySelector('.menu_burger');
+var list = document.querySelector('.nav_header');
+burger.addEventListener('click', function () {
+    list.classList.toggle('vidno');
+
 });
 
-var burger = document.querySelector('.menu_burger');
-burger.addEventListener('click', function () {
-
-    alert('Кто сделал клик???');
+var btnPhoto = document.querySelector('.btn_much_foto');
+var gridPhoto = document.querySelector('.box3_content');
+var allPhotoHeight = document.querySelector('.box3_down');
+btnPhoto.addEventListener('click', function () {
+    gridPhoto.classList.toggle('all_grid_photo');
+    allPhotoHeight.classList.toggle('box3_down_media');
+    btnPhoto.classList.toggle('btn_all_foto');
+    if (btnPhoto.textContent == 'скрыть') {
+        btnPhoto.textContent = 'еще фото';
+    } else(btnPhoto.textContent = 'скрыть');
 
 });
